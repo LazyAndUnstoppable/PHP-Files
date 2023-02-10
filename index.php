85296eb48ddf65873ef5a55a5524d7461f38013a
DarkToL <botinok30@gmail.com>
2023-02-10 20:49:06 +0400

    Первое занятие PHP

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+$a = 3;
+$b = 6;
+$c = 2;
+$d = 5;
+$e = 7;
+$f = 1;
+$g = 2;
+
+echo $e + $d + $c + $f + $g;
+echo "Это сумма пяти простых чисел";
+
+
+
+$num = 1012;
+echo ($num / 100) * 30;
+
+
+
+echo $c ** 15;
+
+
+
+$userName = 'Данил'; # поменять
+$result = 2*2; # поменять
+$githubLink = 'https://github.com/LazyAndUnstoppable'; # поменять
+$fav_color = "Синий"; # сделать переменную для любимого цвета
+
+
+
+phpinfo( INFO_ALL);
+?>
+
+
+<a href="index_1.php">uyhguyuhbh</a>
+
+
---
+++ index_1.php
@@ -0,0 +1,33 @@
+<?php
+$userName = 'Данил'; # поменять
+$result = 2*2; # поменять
+$githubLink = 'https://github.com/LazyAndUnstoppable/'; # поменять
+$fav_color = "Синий"; # сделать переменную для любимого цвета
+?>
+
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="./style.css">
+        <title>Занятие 1</title>
+    </head>
+    <body>
+        <div class="card">
+            <h1>Добро пожаловать, <?php echo $userName; ?>!</h1> <!--поменять-->
+            <p>
+                Мой любимый цвет: <b><?php echo $fav_color; ?></b> <!--сделать цвет через переменную, выбрать свой-->
+            </p>
+            <p>Напоминаю, 2*2 будет <?php echo $result; ?></p> <!--тут НЕ менять-->
+            <p>
+                А вот и ссылка на мой
+                <a href="<?= $githubLink ?>">гитхаб</a> <!--тут НЕ менять-->
+                (вау!)
+            </p>
+        </div>
+    </body>
+</html>
+
